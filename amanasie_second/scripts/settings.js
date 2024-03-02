f213ae6070c01905841f585e3dab84dff0c5f9b0
augustvaella <nikkososaku@akane88.sakura.ne.jp>
2024-03-02 17:10:34 +0900

    json dictionary search

--- amanasie_second/scripts/settings.js
+++ amanasie_second/scripts/settings.js
@@ -1,3 +1,7 @@
 const URL_CSV_DICTIONARY_SEJRJP = "../mm/languages/sejrjp/sejrjp_dictionary.csv";
 const URL_CSV_DICTIONARY_BARRJP = "../mm/languages/barrjp/barrjp_dictionary.csv";
 const URL_CSV_DICTIONARY_ROGANRJP = "../mm/languages/roganrjp/roganrjp_dictionary.csv";
+
+const URL_JSON_DICTIONARY_SEJRJP = "../mm/languages/sejrjp/sejrjp_dictionary.json";
+const URL_JSON_DICTIONARY_BARRJP = "../mm/languages/barrjp/barrjp_dictionary.json";
+const URL_JSON_DICTIONARY_ROGANRJP = "../mm/languages/roganrjp/roganrjp_dictionary.json";
