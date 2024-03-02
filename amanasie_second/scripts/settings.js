cd63081a459430082657b191676f4385540613f1
augustvaella <nikkososaku@akane88.sakura.ne.jp>
2024-03-03 07:52:25 +0900

    rjpron scripts papth

--- amanasie_second/scripts/settings.js
+++ amanasie_second/scripts/settings.js
@@ -2,11 +2,3 @@ const URL_HOME = "http://akane121.sakura.ne.jp/amanasie_second";
 
 const URL_MARKDOWN_FOLDER = `${URL_HOME}/md`;
 const URL_MARKDOWN_INDEX = `${URL_MARKDOWN_FOLDER}/index.md`;
-
-const URL_CSV_DICTIONARY_SEJRJP = "../mm/languages/sejrjp/sejrjp_dictionary.csv";
-const URL_CSV_DICTIONARY_BARRJP = "../mm/languages/barrjp/barrjp_dictionary.csv";
-const URL_CSV_DICTIONARY_ROGANRJP = "../mm/languages/roganrjp/roganrjp_dictionary.csv";
-
-const URL_JSON_DICTIONARY_SEJRJP = "../mm/languages/sejrjp/sejrjp_dictionary.json";
-const URL_JSON_DICTIONARY_BARRJP = "../mm/languages/barrjp/barrjp_dictionary.json";
-const URL_JSON_DICTIONARY_ROGANRJP = "../mm/languages/roganrjp/roganrjp_dictionary.json";
